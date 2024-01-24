e041d022ce38d17fae8f8c69345971f7dff19dd2
empostigo <emmanuel.postigo@tutanota.com>
2024-01-24 16:52:19 +0100

    Create header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,33 @@
+// React Router
+import { Link } from "react-router-dom"
+
+// assets
+import argentBankLogo from "../../assets/argentBankLogo.png"
+
 // Style
 import headerStyle from "./Header.module.scss"
 
 const Header = () => {
   return (
     <header>
-      <nav className="main-nav"></nav>
-      <div></div>
+      <nav className={headerStyle.mainNav}>
+        <Link
+          to="/"
+          className={`${headerStyle.homeLink} ${headerStyle.logoLink}`}
+        >
+          <img
+            className={headerStyle.logo}
+            src={argentBankLogo}
+            alt="Argent Bank Logo"
+          />
+          <h1 className={headerStyle.srOnly}></h1>
+        </Link>
+        <div>
+          <Link to="/sign-in" className={headerStyle.item}>
+            <i className="fa fa-user-circle">Sign In</i>
+          </Link>
+        </div>
+      </nav>
     </header>
   )
 }

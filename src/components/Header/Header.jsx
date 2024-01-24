a523269318d799cf51054bbf431cbb902944c7bd
empostigo <emmanuel.postigo@tutanota.com>
2024-01-24 15:28:35 +0100

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+// Style
+import headerStyle from "./Header.module.scss"
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="main-nav"></nav>
+      <div></div>
+    </header>
+  )
+}
+
+export default Header
